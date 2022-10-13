757f0c45c8190cb5c1bf64c9638b602101de5d43
mesho-17 <103313805+mesho-17@users.noreply.github.com>
2022-10-13 16:11:55 +0300

    Update brute-pss.py

--- brute-pss.py
+++ brute-pss.py
@@ -1,7 +1,7 @@
 import random  #random variable generators
-import pyautogui
+import pyautogui #an automation library that allows mouse and keyboard control.
 import string #collection of string constants. 
-import pyttsx3
+import pyttsx3 #python module to convert text to speech.
 # chars1 = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ #`~!@$%^&*()_+|}{':?>< -=\][';/.,"
 # chas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 # bat = 'tab'
@@ -11,25 +11,25 @@ engine = pyttsx3.init()
 chars = string.printable 
 chars_list = list(chars) 
 
-def speak(text):
+def speak(text): #function name is speak and parameter is text.
     engine.say(text)
-    engine.runAndWait()
+    engine.runAndWait() #if the engine is currently busy speaking an utterance or not. Returns: True if speaking, false if not.
 
 # print(chars1)
-print('Hey, Puny Ass lambistic shit, Enter your password now...')
-speak('Hey, Puny Ass lambistic shit, Enter your password now')
+print('Hey, Enter your password now...')
+speak('Hey, Enter your password now')
 password = pyautogui.password("Enter a password:")
 
 guess_password = " "
 
 while(guess_password != password): 
-    guess_password = random.choices(chars_list, k=len(password))  #len > returns the number of items in a container
+    guess_password = random.choices(chars_list, k=len(password))  #len -> returns the number of items in a container
     # guess_password = random.choices(chas, k=len(password))
     # guess_password = random.choices(bat, k=len(password))
 
-    print("\t\t<=================" + str(guess_password) + "==================>") #str > converts the input to a string 
+    print("\t\t<=================" + str(guess_password) + "==================>") #str -> converts the input into a string 
 
-    if(guess_password == list(password)): #list > if no argument is given, the constructor creates a new empty list.
+    if(guess_password == list(password)): #list -> if no argument is given, the constructor creates a new empty list.
         print('your password is:' + "".join(guess_password)) #The argument must be an iterable if specified.
-        speak('your password is : ' + "".join(guess_password)) #.join > joins strings together into one.
+        speak('your password is : ' + "".join(guess_password)) #.join -> joins strings together into one.
         break
